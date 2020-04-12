b9a4b4177f90e8c24753b2a20c15dbf69e34fa98
daoud010 <daoud010@gmail.com>
2020-04-11 23:04:17 -0400

    Implementing module pattern

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-﻿console.log("hello world!")
+﻿//BUDGET CONTROLLER
+
+var budgetController = (function () {
+
+    // some code
+
+})();
+
+
+// UI CONTROLLER
+
+var UIController = (function () {
+
+
+    // some code
+
+
+
+
+}());
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+    
+
+})(budgetController, UIController);
