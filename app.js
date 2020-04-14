049d468d2ec5d7761a8c1b2eba2abf4148375478
daoud010 <daoud010@gmail.com>
2020-04-14 04:06:55 -0400

    created an in initialization function

--- app.js
+++ app.js
@@ -11,16 +11,81 @@ var budgetController = (function () {
 
 var UIController = (function () {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
-    // some code
+    return {
+        getInput: function () {
+
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+               };
+
+            },
+
+            getDOMstrings: function() {
+                return DOMstrings;
+            }
+    };
 
 
+})();
 
-
-}());
+// GLOBAL APP CONTROLLER
 
 var controller = (function (budgetCtrl, UICtrl) {
 
-    
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
 
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+
+        });
+
+
+    };
+
+
+
+   
+
+    var ctrlAddItem = function () {
+
+
+        // 1. get the filed input data //
+
+        var input = UICtrl.getInput();
+        console.log(input);
+        // 2 . Add the item to the budget controller //
+        // 3. Add the item to the UI//
+        // 4. Calculutate the budget//
+        // 5. Display the Budget on the UI//
+
+
+
+    };
+
+    return {
+        init: function () {
+            console.log('Application has started');
+            setupEventListeners();
+        }
+    };
+   
+    
 })(budgetController, UIController);
+
+
+controller.init();
